797df6aa6dea3a6ac1334d27fb7f92c55ca2ebd4
Dephanie Ho <dephho88@hotmail.com>
2017-09-05 01:20:49 -0700

    Portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+
+class Header extends Component {
+  render() {
+    return(
+      <div>
+        <h1 id="header-title">
+          Mathematics - Computer Science student at
+          University of California, San Diego
+        </h1>
+
+        {/* German Shepherd icon by Icons8 */}
+        <img src="https://png.icons8.com/german-shepherd/color/96"
+          alt= "German Shepherd" id="dog-icon"/>
+
+        <div id="buttons" className="text-center">
+          {/* Github Button */}
+          <a href="https://github.com/Deph-Ho" className="button"
+            target = "_blank" alt="Check out my GitHub!">
+            <FontAwesome name="github" size="2x"/>
+          </a>
+
+          {/* LinkedIn Button */}
+          <a href="https://www.linkedin.com/in/dephanie-ho" className="button"
+            target = "_blank" alt="Check out my LinkedIn!">
+            <FontAwesome name="linkedin" size="2x"/>
+          </a>
+        </div>
+
+        {/* Description */}
+        <div className= "col-md-3"> </div>
+        <div className="col-md-6 textcenter">
+          <h3>
+            Incoming third year student and junior software developer seeking
+            opportunities to develop professional skills and provide a strong
+            contribution to organization goals.
+          </h3>
+        </div>
+        <div className = "col-md-3"> </div>
+      </div>
+    );
+  }
+}
+export default Header;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+
+class Portfolio extends Component {
+  render() {
+    return(
+      <div>
+        <div className="col-md-2"> </div>
+        <div className="col-md-8">
+          <h1 id="portfolio-title">
+            Portfolio
+            <FontAwesome name="folder-open-o" size="lg" id="folder-logo"/>
+          </h1>
+
+          {/* First Row */}
+          <div className="portfolio-image">
+            <h2 id="left-title">
+              Nudge
+              <FontAwesome name="hand-o-up" size="lg" id="font-padding"/>
+            </h2>
+
+            <h2 id="right-title">
+              Instaphoto
+              <FontAwesome name="instagram" size="lg" id="font-padding"/>
+            </h2>
+
+            <a href="https://www.youtube.com/watch?v=gGd28bofXXA&t=3s" target = "_blank">
+              <img className="left-img" id="nudge-img"
+                src="https://camo.githubusercontent.com/529822d63d730bda775da09ed82e031d7ae8d314/687474703a2f2f692e696d6775722e636f6d2f4f4d584f36474d2e676966"
+                alt="Youtube link to walkthrough of Nudge"/>
+            </a>
+
+            <a href="https://github.com/Deph-Ho/Instaphoto" target = "_blank">
+              <img className="right-img"
+                src="https://camo.githubusercontent.com/3d66a972223d3431a335670676e5df20e56dbb9b/687474703a2f2f692e696d6775722e636f6d2f567231347872422e676966"
+                alt="Github Link to Instaphoto"/>
+            </a>
+          </div>
+
+          {/* Second Row */}
+          <div className="portfolio-image">
+            <h2 id="left-title">
+              BlueJayChirp
+              <FontAwesome name="twitter" size="lg" id="font-padding"/>
+            </h2>
+
+            <h2 id="right-title">
+              Foodie-Yelp
+              <FontAwesome name="yelp" size="lg" id="font-padding"/>
+            </h2>
+            <a href = "https://github.com/Deph-Ho/Twitter" target = "_blank">
+              <img className="left-img"
+                src="https://camo.githubusercontent.com/30d13997becad71b0a1908bc903ce3acf1d2fa0f/687474703a2f2f692e696d6775722e636f6d2f327445376a6b512e676966"
+                alt="Github Link to BirdJayChirp"/>
+            </a>
+
+            <a href = "https://github.com/Deph-Ho/Foodie-Yelp" target = "_blank">
+             <img className="right-img"
+               src="https://camo.githubusercontent.com/95346f6c2c44aa886354b959aa948f29547c97ea/687474703a2f2f692e696d6775722e636f6d2f436c524b6269562e676966"
+               alt="Github Link to Foodie-Yelp"/>
+            </a>
+          </div>
+        </div>
+        <div className="col-md-2"> </div>
+      </div>
+    );
+  }
+}
+export default Portfolio;
